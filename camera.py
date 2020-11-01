b640b5e1d9f40dd893f43410f5c3f5b3f9708ff6
SHahab <shahab.1369@gmail.com>
2020-11-01 10:39:07 +0330

    set date folder and pic with name of time

--- camera.py
+++ camera.py
@@ -2,11 +2,31 @@
 import picamera
 from time import sleep
 from picamera import PiCamera
+import os.path
+from datetime import datetime
+
+# varible 
+directory = "/var/www/html/camera/"
+
+
+# check main directory folder existeing
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# generate date an time folder
+today = datetime.now()
+todayeFolder = today.strftime("%Y-%m-%d")
+if not os.path.exists(directory+todayeFolder):
+    os.makedirs(directory+todayeFolder)
+
+picaddress = directory+todayeFolder+"/"+today.strftime('%H%M%S')+".jpg"
+
+#print(picaddress)
 
 camera = PiCamera()
 camera.resolution = (2560,1936)
 camera.rotation = 90
 camera.start_preview()
 sleep(5)
-camera.capture('/var/www/html/pic/test.jpg')
+camera.capture(picaddress)
 camera.stop_preview()
