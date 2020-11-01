96de9f33d869ae793e442da486b7727bb79af90e
SHahab <shahab.1369@gmail.com>
2020-11-01 08:12:00 +0330

    add camera.py file

---
+++ camera.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+import picamera
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (2560,1936)
+camera.start_preview()
+sleep(5)
+camera.capture('/var/www/html/pic/test.jpg')
+camera.stop_preview()
